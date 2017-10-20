2db5a8a40aa662b2bae2394fd55c4ea9099be631
Miangame <miangame1@gmail.com>
2017-10-20 19:21:52 +0200

    Ejercicio android merienda

--- practicas/Android/Merienda/app/src/main/java/com/example/miguel/merienda/MainActivity.java
+++ practicas/Android/Merienda/app/src/main/java/com/example/miguel/merienda/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.miguel.merienda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView vText;
+    private ImageView imagen;
+    private Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vText = (TextView) findViewById(R.id.mensaje);
+        imagen = (ImageView) findViewById(R.id.imagen);
+        boton = (Button) findViewById(R.id.boton);
+
+        boton.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                vText.setText("Estoy lleno!");
+                imagen.setImageResource(R.drawable.after_cookie);
+            }
+        });
     }
 }
